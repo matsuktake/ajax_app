76294d72ec1822b83b51b518d7ac432e895cc309
matsuktake <r.m.fast0208@gmail.com>
2021-11-03 22:28:31 +0900

    JSでレスポンスを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
 
 
   def create
-    post = Post.create(content: oarams[:content])
+    post = Post.create(content: params[:content])
     render json:{ post: post }
   end
 end
